ff8f1f1e8dd12fd0061abf8660d3409c3edebfcc
abhi4578 <29863835+abhi4578@users.noreply.github.com>
2018-11-29 09:16:01 +0530

    commit

--- build.sh
+++ build.sh
@@ -1,5 +1,1 @@
-gcc -fopenmp omp.c -lm -lgsl -lgslcblas -o omp
-gcc -fopenmp omp_parallel.c -lm -lgsl -lgslcblas -o omp_parallel
-mpicc -fopenmp mpiomp.c -lm -lgsl -lgslcblas -o mpiomp
-mpicc mpipso.c -lm -lgsl -lgslcblas -o mpi
-gcc serialpso.c -lm -lgsl -lgslcblas -o serial
+gcc -fopenmp omp.c -lm -lgsl -lgslcblas -o omp ; gcc -fopenmp omp_parallel.c -lm -lgsl -lgslcblas -o omp_parallel ;mpicc -fopenmp mpiomp.c -lm -lgsl -lgslcblas -o mpiomp ;mpicc mpipso.c -lm -lgsl -lgslcblas -o mpi ;gcc serialpso.c -lm -lgsl -lgslcblas -o serial;
